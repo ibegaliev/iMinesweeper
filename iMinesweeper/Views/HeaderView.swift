cb5b35d05a3f77379e0eb688d794838a6d12b899
ibegaliev <81876891+ibegaliev@users.noreply.github.com>
2022-01-05 23:47:28 +0500

    build headerView

---
+++ iMinesweeper/Views/HeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  HeaderView.swift
+//  iMinesweeper
+//
+//  Created by ibrohim begaliev on 1/5/22.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = .black
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+}
