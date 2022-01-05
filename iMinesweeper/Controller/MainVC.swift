6a8085150d60cb3677bc456c8d2aeddc67af16ed
ibegaliev <81876891+ibegaliev@users.noreply.github.com>
2022-01-06 00:21:09 +0500

    build game view

--- iMinesweeper/Controller/MainVC.swift
+++ iMinesweeper/Controller/MainVC.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var headerView: HeaderView!
+    @IBOutlet weak var gameView: GameView!
+    @IBOutlet weak var tabView: UIView!
+    
+    @IBOutlet weak var backgroundView: UIView!{
+        didSet {
+            backgroundView.backgroundColor = AppColor.shared.spaceColor
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .black
     }
     
     
--- iMinesweeper/Views/HeaderView.swift
+++ iMinesweeper/Views/HeaderView.swift
@@ -9,13 +9,53 @@ import UIKit
 
 class HeaderView: UIView {
     
+    var containerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "background")
+        return imageView
+    }()
+    
+    var containerView: UIView = {
+        let cview = UIView()
+        cview.translatesAutoresizingMaskIntoConstraints = false
+        cview.backgroundColor = AppColor.shared.spaceColor
+        return cview
+    }()
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        backgroundColor = .black
+        setBackground()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+    }
+    
+    
+    
+}
+
+
+extension HeaderView {
+    
+    func setBackground(){
+        addSubview(containerImageView)
+        NSLayoutConstraint.activate([
+            containerImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            containerImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            containerImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            containerImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+        
+        addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 4),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -4)
+        ])
         
     }
     
