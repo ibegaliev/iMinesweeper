875619a0fd46bda9f1d147e91aa4187b18176a44
ibegaliev <81876891+ibegaliev@users.noreply.github.com>
2022-01-05 15:59:46 +0500

    app color and fond

---
+++ iMinesweeper/Model/AppColor.swift
@@ -0,0 +1,18 @@
+//
+//  AppColor.swift
+//  iMinesweeper
+//
+//  Created by ibrohim begaliev on 1/5/22.
+//
+
+import UIKit
+
+struct AppColor {
+//    MARK: shared
+    static var shared = AppColor()
+    
+    var whiteColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var spaceColor = #colorLiteral(red: 0.7490196824, green: 0.7490196824, blue: 0.7490196824, alpha: 1)
+    var darkColor = #colorLiteral(red: 0.5058823228, green: 0.5058825016, blue: 0.510187149, alpha: 1)
+    
+}
